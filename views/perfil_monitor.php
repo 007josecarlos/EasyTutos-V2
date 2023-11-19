a3d1a0c78f5b9eb972dc7db4a92cc4935cefbd8d
007josecarlos <45445559+007josecarlos@users.noreply.github.com>
2023-11-19 11:43:00 -0500

    Update perfil_monitor.php

--- views/perfil_monitor.php
+++ views/perfil_monitor.php
@@ -1,6 +1,52 @@
-<?php session_start(); ?>
+<?php 
+  session_start(); 
+
+?> 
+  
 <?php include 'header.php'; ?>
-<?php include 'nav.php'; ?>
+<!-- Barra de navegación dasbohar -->
+<nav class="navbar navbar-expand-lg navbar-custom">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../public/img/logo.png" width="40" height="40" class="d-inline-block align-text-top">
+      EasyTutos
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+
+      </ul>
+
+      <ul class="navbar-nav ml-auto me-5">
+        <li class="nav-item">
+          <a class="nav-link px-3" href="#">
+            <span class="material-icons">notifications</span>
+            <span class="badge badge-pill badge-warning position-absolute">10</span>
+          </a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav me-5">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://randomuser.me/api/portraits/men/94.jpg" alt="John Doe" width="40" class="border mp-1 rounded-circle">
+            <?php if (isset($_SESSION['usuario'])){ echo $_SESSION['nombre'];}?>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#"><i class="fas fa-user fa-fw"></i> Profile</a></li>
+            <li><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-fw"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+
 <!-- Título de la sección -->
 <div class="container mt-5 d-flex flex-column align-items-center">
   <h2>Tu perfil</h2>
@@ -10,7 +56,7 @@
   <div class="row">
     <!-- Datos monitor -->
     <div class="col-sm-6">
-      <h4>Nombre: Jhon Doe</h4>
+      <h4>Nombre:</h4>
       <h4>Asignaturas: Sistemas operativos II</h4>
       <h4>Correo electronico: alguien@domain.com</h4>
       <h4>Numero de telefono: 123 45678910</h4>
@@ -23,7 +69,7 @@
   </div>
   <div class="container mt-5 mb-5">
     <div class="twoButtons">
-        <button class="blue-button" href="editar_perfil_monitor.php">Editar perfil</button>
+        <button class="blue-button" onclick="window.location.href='editar_perfil_monitor.php'">Editar perfil</button>
         <button class="orange-button">Eliminar perfil</button>
     </div>
   </div>
